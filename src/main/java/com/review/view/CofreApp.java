5cff124286c3ff2e35e07da424a574e9e6c813ca
Pedro Lopes <pedro.lopesverardo@gmail.com>
2025-05-18 16:14:08 -0300

    add button logic

--- src/main/java/com/review/view/CofreApp.java
+++ src/main/java/com/review/view/CofreApp.java
@@ -12,6 +12,8 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
 
+import java.util.List;
+
 public class CofreApp extends Application {
 
     private Stage primaryStage;
@@ -102,13 +104,105 @@ public class CofreApp extends Application {
 
         HBox campoLogin = new HBox(10, new Label("Login: "), new TextField());
         HBox campoFraseSecreta = new HBox(10, new Label("Frase secreta: "), new TextField());
-        HBox campoSenha = new HBox(10, new Label("Senha: "), new TextField());
-        HBox campoConfirmacaoSenha = new HBox(10, new Label("Confirmação Senha: "), new TextField());
 
-        VBox layout = new VBox(20,titleLabel, campoLogin, campoFraseSecreta, campoSenha, campoConfirmacaoSenha);
+        Label senhaLabel = new Label("Senha (selecione usando os botões): ");
+
+        TextField senhaDisplay = new TextField();
+        senhaDisplay.setEditable(false);
+
+        List<Integer> botoesPressionados = new java.util.ArrayList<>();
+
+        List<Integer> valores = new java.util.ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            valores.add(i);
+        }
+        java.util.Collections.shuffle(valores);
+
+        java.util.Map<Integer, int[]> valoresPorBotao = new java.util.HashMap<>();
+
+        HBox botoesContainer = new HBox(10);
+        List<Button> botoesSenha = new java.util.ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            final int indiceButton = i;
+            final int valor1 = valores.get(i * 2);
+            final int valor2 = valores.get(i * 2 + 1);
+
+            valoresPorBotao.put(indiceButton, new int[]{valor1, valor2});
+
+            Button botao = new Button(valor1 + "-" + valor2);
+
+            botao.setOnAction(e -> {
+                botoesPressionados.add(indiceButton);
+
+                senhaDisplay.setText(senhaDisplay.getText() + "*");
+            });
+
+            botoesSenha.add(botao);
+            botoesContainer.getChildren().add(botao);
+        }
+
+        Button limparSenha = new Button("Limpar");
+        limparSenha.setOnAction(e -> {
+            botoesPressionados.clear();
+            senhaDisplay.setText("");
+        });
+        botoesContainer.getChildren().add(limparSenha);
+
+        Button loginButton = new Button("Entrar");
+        loginButton.setOnAction(e -> {
+            String login = ((TextField) campoLogin.getChildren().get(1)).getText();
+            String fraseSecreta = ((TextField) campoFraseSecreta.getChildren().get(1)).getText();
+
+            // TODO Verificar se a senha é válida - teste atual
+            String senhaReal = "1234";
+
+            if (verificarSenha(senhaReal, botoesPressionados, valoresPorBotao)) {
+                System.out.println("Login bem-sucedido!");
+            } else {
+                System.out.println("Senha inválida!");
+            }
+
+        });
+
+        HBox campoSenha = new HBox(10, senhaLabel, senhaDisplay);
+
+        VBox layout = new VBox(20, titleLabel, campoLogin, campoFraseSecreta, campoSenha, botoesContainer, loginButton);
         layout.setAlignment(javafx.geometry.Pos.CENTER);
 
-        Scene scene = new Scene(layout, 350, 350);
+        Scene scene = new Scene(layout, 500, 400);
         primaryStage.setScene(scene);
+    }
+
+
+    private boolean verificarRecursivo(String senhaReal, int posicao, List<Integer> botoesPressionados,
+                                       java.util.Map<Integer, int[]> valoresPorBotao, String senhaAtual) {
+        if (posicao == botoesPressionados.size()) {
+            return senhaAtual.equals(senhaReal);
+        }
+
+        int botao = botoesPressionados.get(posicao);
+
+        int[] valores = valoresPorBotao.get(botao);
+
+        // Tentar com o primeiro valor
+        if (verificarRecursivo(senhaReal, posicao + 1, botoesPressionados, valoresPorBotao,
+                senhaAtual + valores[0])) {
+            return true;
+        }
+
+        // Tentar com o segundo valor
+        return verificarRecursivo(senhaReal, posicao + 1, botoesPressionados, valoresPorBotao,
+                senhaAtual + valores[1]);
+    }
+
+    private boolean verificarSenha(String senhaReal, List<Integer> botoesPressionados, java.util.Map<Integer, int[]> valoresPorBotao) {
+        // Verificar se a quantidade de botões pressionados corresponde ao tamanho da senha
+        if (botoesPressionados.size() != senhaReal.length()) {
+            return false;
+        }
+
+        // Lista de todas as possíveis combinações de senha
+        return verificarRecursivo(senhaReal, 0, botoesPressionados, valoresPorBotao, "");
     }
 }
